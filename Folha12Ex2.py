d9be7a5684a87389bad0cb22f7d7d11482d18743
Miguel M <miguelmurca@gmail.com>
2018-05-25 18:26:53 +0100

    Thanks for all the fish

--- Folha12Ex2.py
+++ Folha12Ex2.py
@@ -5,56 +5,61 @@
 from random import randint, choice
 from scipy.stats import chi2
 
+XI2C = chi2.isf(q=0.05, df=5)
+
+
 def honest():
-    return randint(1,6)
+    return randint(1, 6)
+
 
 def loaded():
-    return choice(tuple(range(1,6)) + (6,)*2)
+    return choice(tuple(range(1, 6)) + (6,)*2)
+
 
 def is_fair(func, throws):
-    ONE_SIXTH = 1/6
     freq = [0]*6
     for _ in range(throws):
         side = func()
         freq[side - 1] += 1
-    x2 = sum((freq[i] - ONE_SIXTH)**2/ONE_SIXTH for i in range(len(freq)))
-    if x2 < chi2.isf(q = 0.05, df = 5):
+    x2 = sum((freq[i] - throws/6)**2*6/throws for i in range(len(freq)))
+    if x2 < XI2C:
         return True
     return False
 
+
 if __name__ == '__main__':
-    
+
     import matplotlib.pyplot as plt
     import numpy as np
-    import threading
+    import concurrent.futures
+    from time import sleep
 
     THROWS = 1000
-    RUNS = 10000
-    TOTAL = THROWS*RUNS
+    THROW_STEP = 20
+    RUNS = 100
 
-    points = np.zeros((THROWS, 2))
+    point_range = np.arange(THROW_STEP, THROWS, THROW_STEP)
+    points = np.zeros((len(point_range), 2))
 
     def run(throws):
         global points
-        local = threading.local()
-        local.fair_truths = 0
-        local.loaded_truths = 0
+        fair_truths = 0
+        loaded_truths = 0
         for _ in range(RUNS):
             if is_fair(honest, throws):
-                local.fair_truths += 1
+                fair_truths += 1
             if is_fair(loaded, throws):
-                local.loaded_truths += 1
-        points[throws,:] = (local.fair_truths/RUNS, local.loaded_truths/RUNS)
+                loaded_truths += 1
+        points[int(throws/THROW_STEP) - 1, :] = (fair_truths /
+                                             RUNS, loaded_truths/RUNS)
 
-    threads = []
-    for throws in range(THROWS):
-        t = threading.Thread(target=run, args=(throws,))
-        threads.append(t)
-        t.start()
-    
-    for t in threads:
-        print('Done')
-        t.join()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=200) as executor:
+        for throws in point_range:
+            executor.submit(run, throws)
 
-    plt.plot(np.arange(THROWS), points, 'o')
-    plt.show()        
+    plt.title('~Probability of fairness for N throws')
+    plt.xlabel('N. of throws')
+    plt.plot(point_range, points[:,0], 'o', label='Fair die')
+    plt.plot(point_range, points[:,1], 'o', label='Loaded die')
+    plt.legend(loc='center right')
+    plt.show()
